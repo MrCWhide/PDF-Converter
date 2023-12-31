8cf74532536bd8a67bb2f12903a43fb4cbe7679c
MrCWhide <141937887+MrCWhide@users.noreply.github.com>
2023-12-31 06:40:56 +0000

    VSCODE更新

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 # -*- coding: utf-8 -*-
 #CODED BY CWhide  20231231
-
 from pdf2docx import Converter
-
 pdf_file=r'test.pdf'
 docx_file=r'test.docx'
 Converter(pdf_file).convert(docx_file,start=0,end=None)
